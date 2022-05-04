ba515a457768d6f205e51c9b9f2b7b00d1e227b1
Kevin Chaney <kevin_chaney@icloud.com>
2022-05-03 22:51:25 -0700

    commit tree bd9bbaea782a520c5a886d93362ef28b4e8c341e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+var bestCustomer;
+const leastFavoriteCustomer = 0;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+    console.log(bestCustomer);
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 1;
+    return leastFavoriteCustomer;
+}
